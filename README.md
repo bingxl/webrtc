be712b2467e6d0c96e9585b8209e9e4467504bae
bingxl <2282152858@qq.com>
2018-04-11 20:03:30 +0800

    init and get media, init peerConnect

---
+++ README.md
@@ -0,0 +1,7 @@
+# webRTC test
+## pre work
+install parcel， use yarn or npm 
+`yarn global add parcel-bundler` `npm install parcel-bbundleruild -g`
+
+## run command
+`parcel index.html` 
---
+++ main.js
@@ -0,0 +1,91 @@
+
+let yourVideo = document.querySelector('#yours');
+let theirVideo = document.querySelector('#theirs');
+let yourConnection, theirConnection;
+
+// Put variables in global scope to make them available to the browser console.
+let constraints = window.constraints = {
+  audio: false,
+  video: true
+};
+
+let offerOptions = {
+  offerToReceiveAudio: 0,
+  offerToReceiveVideo: 1
+};
+
+navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
+
+function handleSuccess(stream) {
+//   var videoTracks = stream.getVideoTracks();
+  let audioTracks = stream.getAudioTracks();
+  console.log('Got stream with constraints:', constraints);
+  window.stream = stream; // make variable available to browser console
+  yourVideo.srcObject = stream;
+  startPeerConnection(stream);
+}
+
+function handleError(error) {
+  if (error.name === 'ConstraintNotSatisfiedError') {
+    errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
+        constraints.video.width.exact + ' px is not supported by your device.');
+  } else if (error.name === 'PermissionDeniedError') {
+    errorMsg('Permissions have not been granted to use your camera and ' +
+      'microphone, you need to allow the page access to your devices in ' +
+      'order for the demo to work.');
+  }
+  errorMsg('getUserMedia error: ' + error.name, error);
+}
+
+function startPeerConnection(stream) {
+  console.log('start Connection function');
+  let configuration = {
+    // iceServers: [{url: "stun:127.0.0.1:9874"}]
+  };
+
+  yourConnection = new RTCPeerConnection(configuration);
+  theirConnection = new RTCPeerConnection(configuration);
+
+  // create ice process
+  yourConnection.onicecandidate = event => {
+    console.log('yourConnection ice candidate event');
+    if(event.candidate) {
+      theirConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+    }
+  };
+
+  theirConnection.onicecandidate = event => {
+    console.log('theirConnection ice candidate event');
+    if(event.candidate) {
+      yourConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+    }
+  };
+
+  theirConnection.ontrack = e => {
+    if(theirVideo.srcObject !== e.streams[0]) {
+      theirVideo.srcObject = e.streams[0];
+    }
+  };
+
+  // add track to local peerconnection
+  stream.getTracks().forEach(track => {
+    yourConnection.addTrack(track, stream);
+  });
+
+  // strart to offer
+  yourConnection.createOffer(offerOptions).then(offer => {
+    console.log('create offer');
+    yourConnection.setLocalDescription(offer);
+    theirConnection.setRemoteDescription(offer);
+
+    theirConnection.createAnswer().then( answer => {
+      theirConnection.setLocalDescription(answer);
+      yourConnection.setRemoteDescription(answer);
+      window.answer = answer;
+      console.log('create answer');
+    })
+    window.offer = offer;
+  });
+
+}

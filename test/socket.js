abdd1b2f1a0a69890ff66a3ed39420ad25c8781f
bingxl <2282152858@qq.com>
2018-04-12 23:40:19 +0800

    use tls

--- test/socket.js
+++ test/socket.js
@@ -1,6 +1,7 @@
 let WebSocketServer = require('ws').Server;
 let fs = require("fs");
 let https = require('https');
+let tls = require("tls");
 
 let keypath = __dirname + '/server.key';
 let certpath = __dirname + '/server.crt';
@@ -10,7 +11,8 @@ let options = {
     cert: fs.readFileSync(certpath),
     rejectUnauthorized: false
 };
-let server = https.createServer(options,(req, res) => {
-    res.writeHead(200);
-    res.end("this is a websocket server \n");
+let server = tls.createServer(options,(stream) => {
+    stream.write("welecome \n");
+    stream.setEncoding('utf8');
+    stream.pipe(stream);
 }).listen(8888);
